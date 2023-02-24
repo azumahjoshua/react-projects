ef80285a5164e74c7d9b17f08b6742cdd38788ff
azumahjoshua <joshuaazumah1086@gmail.com>
2023-02-24 17:06:22 +0100

    birthday project done

--- 01-birthday-reminder/setup/src/List.js
+++ 01-birthday-reminder/setup/src/List.js
@@ -1,11 +1,23 @@
-import React from 'react';
-
-const List = () => {
-  return (
-    <>
-      <h2>list component</h2>
-    </>
-  );
+import React from "react";
+// import data from "./data";
+const List = ({ people }) => {
+	return (
+		<>
+			<ul></ul>
+			{people.map((person) => {
+				const { id, name, age, image } = person;
+				return (
+					<article key={id} className='person'>
+						<img src={image} alt={name} />
+						<div>
+							<h4>{name}</h4>
+							<p>{age} years</p>
+						</div>
+					</article>
+				);
+			})}
+		</>
+	);
 };
 
 export default List;
